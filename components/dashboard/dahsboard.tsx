bdd5b15604df4bcdce79f81f8e64e6db338b054d
SpaceJack777 <laurynas.space@gmail.com>
2026-02-01 19:30:04 +0200

    commit 1

--- components/dashboard/dahsboard.tsx
+++ components/dashboard/dahsboard.tsx
@@ -4,7 +4,7 @@ import { RecentHabits } from "./habits";
 
 export function Dashboard() {
   return (
-    <div className="grid grid-cols-1 gap-4 md:grid-cols-2 2xl:grid-cols-3">
+    <div className="grid grid-cols-1 gap-4 lg:grid-cols-2 2xl:grid-cols-3">
       <FocusStats />
       <RecentNotes />
       <RecentHabits />
